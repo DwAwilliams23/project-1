d321e5fcd843ab2c66ee70deb645e9ab34a5d10e
DwAwilliams23 <120130350+DwAwilliams23@users.noreply.github.com>
2023-01-17 08:42:38 -0500

    Setting up the board

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log('Battleship')
+document.addEventListener('DOMContentLoaded', () => {
+    //All Constants
+    const userGrid = document.querySelector('.grid-user')
+    const computerGrid = document.querySelector('.grid-computer')
+    const userSquares = []
+    const computerSquares = []
+    const width = 10
+
+    //Function for creating the board
+    function createBoard(grid, squares) {
+        for (let i = 0; i < width*width; i++) {
+            const square = document.createElement('div')
+            square.dataset.id = i
+            grid.appendChild(square)
+            squares.push(square)
+        }
+  }
+  createBoard(userGrid, userSquares)
+  createBoard(computerGrid, computerSquares)
+})
